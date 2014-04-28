b41f3339c32ce64dc51fa77752deeb5add988a8c
Ian Kirker <ian.kirker@gmail.com>
2014-04-28 15:58:22 +0100

    GPLv2ed

--- UCLRC.php
+++ UCLRC.php
@@ -1,4 +1,23 @@
 <?php
+/*  UCL MediaWiki Skin
+ *  Copyright (C) 2014  UCL
+ *  Contains elements that are copyright of the Mediawiki foundation, and
+ *   also elements that are copyright 1999-2014 UCL
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 /**
  * UCLRC skin
  *
